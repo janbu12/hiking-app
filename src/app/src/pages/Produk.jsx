4b35659846bb3b28205a1ec03955e508d3b39cab
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-11-14 22:30:17 +0700

    refactor produk.jsx, sewa.jsx for loading state

--- src/app/src/pages/Produk.jsx
+++ src/app/src/pages/Produk.jsx
@@ -4,15 +4,20 @@ import { useParams } from 'react-router-dom'
 export default function Produk() {
     const [data, setData] = useState([]);
     const {id} = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+
 
     const fetchData = async (id) => {
         try {
+            setIsLoading(true);
             const response = await fetch(`${import.meta.env.VITE_BACKEND_URL_LOCAL}/productsRents/${id}`);
             const {data} = await response.json();
             setData(data);
             console.log(data);
         } catch (error) {
             console.error('Error:', error);
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -24,29 +29,35 @@ export default function Produk() {
     console.log(data);
     
     return (
-        <>
-            {data.map((data)=>{
-                return (
-                    <div key={data.produk_id}>
-                        <h1>{data.nama_produk}</h1>
-                        <p>{data.dekripsi}</p>
-                        <p>Harga: Rp. {data.harga_sewa}</p>
-                        <p>Stok: {data.stok}</p>
-                        <button>Beli</button>
-                        <button>Tambah ke Keranjang</button>
-                        <div>
-                            <h2>Reviews</h2>
-                            {data.reviews.map((review) => (
-                                <div key={review.review_id}>
-                                    <h3>Rating: {review.rating}</h3>
-                                    <p>Komentar: {review.komentar}</p>
-                                    <p>Tanggal: {review.review_created_at}</p>
+        <> 
+            {isLoading ? (
+                <div>Loading...</div>
+            ):(
+                <>
+                    {data.map((data)=>{
+                        return (
+                            <div key={data.produk_id}>
+                                <h1>{data.nama_produk}</h1>
+                                <p>{data.dekripsi}</p>
+                                <p>Harga: Rp. {data.harga_sewa}</p>
+                                <p>Stok: {data.stok}</p>
+                                <button>Beli</button>
+                                <button>Tambah ke Keranjang</button>
+                                <div>
+                                    <h2>Reviews</h2>
+                                    {data.reviews.map((review) => (
+                                        <div key={review.review_id}>
+                                            <h3>Rating: {review.rating}</h3>
+                                            <p>Komentar: {review.komentar}</p>
+                                            <p>Tanggal: {review.review_created_at}</p>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    </div>                        
-                )
-            })}
+                            </div>                        
+                        )
+                    })}
+                </>
+            )}
         </>
     )
 }
--- src/app/src/pages/Sewa.jsx
+++ src/app/src/pages/Sewa.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function Sewa() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {
       try {
+          setIsLoading(true);
           const response = await fetch(`${import.meta.env.VITE_BACKEND_URL_LOCAL}/productsRents/`);
           const {data} = await response.json();
           setData(data);
           console.log(data);
       } catch (error) {
           console.error('Error:', error);
+      } finally {
+        setIsLoading(false);
       }
   }
 
@@ -23,17 +27,23 @@ export default function Sewa() {
   console.log(data);
     return (
       <>
-        <div>Sewa</div><hr />
-        {
-          data.map((product) => (
-            <div key={product.produk_id}>
-              <h1>{product.nama_produk}</h1>
-              <p>Harga: {product.harga_sewa}</p>
-              <p>Stok: {product.stok}</p>
-              <Link to={`${import.meta.env.VITE_URL_LOCAL}/produk/${product.produk_id}`}>Lihat</Link><hr />
-            </div>
-          ))
-        }
+        {isLoading ? (
+          <div>Loading...</div>
+        ):(
+          <>
+            <div>Sewa</div><hr />
+            {
+              data.map((product) => (
+                <div key={product.produk_id}>
+                  <h1>{product.nama_produk}</h1>
+                  <p>Harga: {product.harga_sewa}</p>
+                  <p>Stok: {product.stok}</p>
+                  <Link to={`${import.meta.env.VITE_URL_LOCAL}/produk/${product.produk_id}`}>Lihat</Link><hr />
+                </div>
+              ))
+            }
+          </>
+        )}
       </>
     )
 }
